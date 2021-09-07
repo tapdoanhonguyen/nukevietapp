e9e73738cf632e48d454de92709272f395ea4655
webvang <hoang.nguyen@webvang.vn>
2021-09-08 06:19:15 +0700

    Create login_vm.dart

---
+++ lib/src/modules/login_vm.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:nukeviet/src/include/base.dart';
+import 'package:nukeviet/src/include/constants.dart';
+import 'package:nukeviet/src/vendor/network/global.dart';
+import 'package:nukeviet/src/vendor/network/response/mapping/user.dart';
+import 'package:nukeviet/src/vendor/network/response/mapping/nv_token.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+class LoginViewModel extends BaseViewModel {
+  final usernameCtrl = TextEditingController();
+  final passwordCtrl = TextEditingController();
+
+  ///
+  String get username => usernameCtrl.text;
+  String get password => passwordCtrl.text;
+
+  bool get isValid => username.isNotEmpty && password.isNotEmpty;
+
+  LoginViewModel() {
+    usernameCtrl.text = r'';
+    passwordCtrl.text = r'';
+  }
+
+  Future<NVToken> login() async {
+    Global.shared.timestamp = Timestamp.fromDate(DateTime.now()).seconds;
+    Global.shared.setDataPost( 'users', 'Login');
+    return api.securityToken(username: username, password: password);
+  }
+
+  Future<User> getUser(int userId) async {
+    Global.shared.timestamp = Timestamp.fromDate(DateTime.now()).seconds;
+    Global.shared.setDataPost( 'users', 'UsersInfo');
+    AppConstant.userId = userId.toString();
+    return api.getUser(userId: userId);
+  }
+}
